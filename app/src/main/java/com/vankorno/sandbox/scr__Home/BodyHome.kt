ac7c8f26416887dd10bbc409ec0e5c32ddd4f858
VanKorno <kornovandrey@gmail.com>
2025-11-29 07:02:39 +0200

    App module work

--- app/src/main/java/com/vankorno/sandbox/scr__Home/BodyHome.kt
+++ app/src/main/java/com/vankorno/sandbox/scr__Home/BodyHome.kt
@@ -10,6 +10,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.vankorno.sandbox.MyApp.Companion.dbh
@@ -72,5 +76,12 @@ private fun NavBtnText(                                                         
         color = LibColor.WhiteText.color,
         fontSize = 30.sp2(),
         fontWeight = FontWeight.Bold,
+        style = TextStyle(
+            shadow = Shadow(
+                color = Color.Black,
+                offset = Offset(7f, 7f),
+                blurRadius = 8f
+            )
+        )
     )
 }
