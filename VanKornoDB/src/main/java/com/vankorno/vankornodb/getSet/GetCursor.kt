776ccddba2cd7de7acc40d05a3a43e10daf74d12
VanKorno <kornovandrey@gmail.com>
2025-12-05 00:16:05 +0200

    Unsafe Backup Commit

---
+++ VanKornoDB/src/main/java/com/vankorno/vankornodb/getSet/GetCursor.kt
@@ -0,0 +1,36 @@
+package com.vankorno.vankornodb.getSet
+/** This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ *  If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+**/
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import com.vankorno.vankornodb.core.QueryOpts
+import com.vankorno.vankornodb.core.getQuery
+
+fun SQLiteDatabase.getCursor(                                        table: String,
+                                                                   columns: Array<out String>,
+                                                                 queryOpts: QueryOpts.()->Unit = {},
+): Cursor {
+    val (query, args) = getQuery(table, columns, queryOpts)
+    return rawQuery(query, args)
+}
+
+
+fun SQLiteDatabase.getCursor(                                        table: String,
+                                                                    column: String = "*",
+                                                                 queryOpts: QueryOpts.()->Unit = {},
+): Cursor {
+    val (query, args) = getQuery(table, arrayOf(column), queryOpts)
+    return rawQuery(query, args)
+}
+
+
+
+/*
+fun SQLiteDatabase.getCursor(                                     table: String,
+                                                                vararg columns: String,
+                                                                queryOpts: QueryOpts.()->Unit = {},
+): Cursor {
+    val (query, args) = getQuery(table, columns)
+    return rawQuery(query, args)
+}*/
