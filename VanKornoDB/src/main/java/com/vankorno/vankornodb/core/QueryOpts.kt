78181b1a26f141626aeb6a7c3d600d0c45e2785d
VanKorno <kornovandrey@gmail.com>
2025-12-05 00:01:29 +0200

    Unsafe Backup Commit

--- VanKornoDB/src/main/java/com/vankorno/vankornodb/core/QueryOpts.kt
+++ VanKornoDB/src/main/java/com/vankorno/vankornodb/core/QueryOpts.kt
@@ -5,9 +5,37 @@ import com.vankorno.vankornodb.core.data.QueryOptsHolder
 class QueryOpts {
     val query = QueryOptsHolder()
     
-    fun joins() {
-        
+    fun joins(builder: JoinBuilder.()->Unit) {
+        query.joins = builder
     }
+    
+    fun where(builder: WhereBuilder.()->Unit) {
+        query.where = builder
+    }
+    
+    fun groupBy(by: String) {
+        query.groupBy = by
+    }
+    
+    fun having(having: String) {
+        query.having = having
+    }
+    fun orderBy(by: String) {
+        query.orderBy = by
+    }
+    fun limit(limit: Int) {
+        query.limit = limit
+    }
+    
+    fun offset(offset: Int) {
+        query.offset = offset
+    }
+    
+    fun customEnd(queryStr: String) {
+        query.customEnd = queryStr
+    }
+    
+    
     
     
     
