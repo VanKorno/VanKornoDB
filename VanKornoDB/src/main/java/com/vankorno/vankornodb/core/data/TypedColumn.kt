8e88c41a838a477f7be2b856b290c44a26c2af10
VanKorno <kornovandrey@gmail.com>
2025-12-07 01:20:45 +0200

    Extend TypedColumn

--- VanKornoDB/src/main/java/com/vankorno/vankornodb/core/data/TypedColumn.kt
+++ VanKornoDB/src/main/java/com/vankorno/vankornodb/core/data/TypedColumn.kt
@@ -1,32 +1,37 @@
 package com.vankorno.vankornodb.core.data
 
-sealed class TypedColumn<T>(val name: String)
+sealed class TypedColumn<T>(
+    val name: String,
+    val defaultVal: T,
+    val size: Int = 1, // default 1 for scalar columns, >1 for lists
+)
 
-class IntCol(name: String) : TypedColumn<Int>(name)
-class StrCol(name: String) : TypedColumn<String>(name)
-class BoolCol(name: String) : TypedColumn<Boolean>(name)
-class LongCol(name: String) : TypedColumn<Long>(name)
-class FloatCol(name: String) : TypedColumn<Float>(name)
-class BlobCol(name: String) : TypedColumn<ByteArray>(name)
+class IntCol(name: String, defaultVal: Int = 0) : TypedColumn<Int>(name, defaultVal)
+class StrCol(name: String, defaultVal: String = "") : TypedColumn<String>(name, defaultVal)
+class BoolCol(name: String, defaultVal: Boolean = false) : TypedColumn<Boolean>(name, defaultVal)
+class LongCol(name: String, defaultVal: Long = 0L) : TypedColumn<Long>(name, defaultVal)
+class FloatCol(name: String, defaultVal: Float = 0F) : TypedColumn<Float>(name, defaultVal)
+class BlobCol(name: String, defaultVal: ByteArray = ByteArray(0)) : TypedColumn<ByteArray>(name, defaultVal)
 
-fun intCol(name: String) = IntCol(name)
-fun strCol(name: String) = StrCol(name)
-fun boolCol(name: String) = BoolCol(name)
-fun longCol(name: String) = LongCol(name)
-fun floatCol(name: String) = FloatCol(name)
-fun blobCol(name: String) = BlobCol(name)
+fun intCol(name: String, defaultVal: Int = 0) = IntCol(name, defaultVal)
+fun strCol(name: String, defaultVal: String = "") = StrCol(name, defaultVal)
+fun boolCol(name: String, defaultVal: Boolean = false) = BoolCol(name, defaultVal)
+fun longCol(name: String, defaultVal: Long = 0L) = LongCol(name, defaultVal)
+fun floatCol(name: String, defaultVal: Float = 0F) = FloatCol(name, defaultVal)
+fun blobCol(name: String, defaultVal: ByteArray = ByteArray(0)) = BlobCol(name, defaultVal)
 
 
-class IntListCol(name: String) : TypedColumn<List<Int>>(name)
-class StrListCol(name: String) : TypedColumn<List<String>>(name)
-class BoolListCol(name: String) : TypedColumn<List<Boolean>>(name)
-class LongListCol(name: String) : TypedColumn<List<Long>>(name)
-class FloatListCol(name: String) : TypedColumn<List<Float>>(name)
-class BlobListCol(name: String) : TypedColumn<List<ByteArray>>(name)
+class IntListCol(name: String, size: Int, defaultVal: Int = 0) : TypedColumn<List<Int>>(name, List(size) { defaultVal }, size)
+class StrListCol(name: String, size: Int, defaultVal: String = "") : TypedColumn<List<String>>(name, List(size) { defaultVal }, size)
+class BoolListCol(name: String, size: Int, defaultVal: Boolean = false) : TypedColumn<List<Boolean>>(name, List(size) { defaultVal }, size)
+class LongListCol(name: String, size: Int, defaultVal: Long = 0L) : TypedColumn<List<Long>>(name, List(size) { defaultVal }, size)
+class FloatListCol(name: String, size: Int, defaultVal: Float = 0F) : TypedColumn<List<Float>>(name, List(size) { defaultVal }, size)
+class BlobListCol(name: String, size: Int, defaultVal: ByteArray = ByteArray(0)) : TypedColumn<List<ByteArray>>(name, List(size) { defaultVal }, size)
 
-fun intListCol(name: String) = IntListCol(name)
-fun strListCol(name: String) = StrListCol(name)
-fun boolListCol(name: String) = BoolListCol(name)
-fun longListCol(name: String) = LongListCol(name)
-fun floatListCol(name: String) = FloatListCol(name)
-fun blobListCol(name: String) = BlobListCol(name)
+
+fun intListCol(name: String, size: Int, defaultVal: Int = 0) = IntListCol(name, size, defaultVal)
+fun strListCol(name: String, size: Int, defaultVal: String = "") = StrListCol(name, size, defaultVal)
+fun boolListCol(name: String, size: Int, defaultVal: Boolean = false) = BoolListCol(name, size, defaultVal)
+fun longListCol(name: String, size: Int, defaultVal: Long = 0L) = LongListCol(name, size, defaultVal)
+fun floatListCol(name: String, size: Int, defaultVal: Float = 0F) = FloatListCol(name, size, defaultVal)
+fun blobListCol(name: String, size: Int, defaultVal: ByteArray = ByteArray(0)) = BlobListCol(name, size, defaultVal)
