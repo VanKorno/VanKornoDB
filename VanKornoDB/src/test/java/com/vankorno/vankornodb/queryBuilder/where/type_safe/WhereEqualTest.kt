b4eb75d0976869f64c54d090c3b8137fa51af28f
VanKorno <kornovandrey@gmail.com>
2025-12-08 20:56:30 +0200

    tests

--- VanKornoDB/src/test/java/com/vankorno/vankornodb/queryBuilder/where/type_safe/WhereEqualTest.kt
+++ VanKornoDB/src/test/java/com/vankorno/vankornodb/queryBuilder/where/type_safe/WhereEqualTest.kt
@@ -1,11 +1,17 @@
 package com.vankorno.vankornodb.queryBuilder.where.type_safe
 
 import com.vankorno.vankornodb.core.WhereBuilder
+import com.vankorno.vankornodb.core.data.DbConstants
+import com.vankorno.vankornodb.core.data.SharedCol.shActive
+import com.vankorno.vankornodb.core.data.SharedCol.shName
 import com.vankorno.vankornodb.core.data.SharedCol.shPosition
+import com.vankorno.vankornodb.core.data.fCol
+import com.vankorno.vankornodb.core.data.lCol
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class WhereEqualTest {
+    
     @Test
     fun `Equal Int`() {
         val where: WhereBuilder.()->Unit = { shPosition equal 11 }
@@ -15,6 +21,46 @@ class WhereEqualTest {
         assertEquals(whereObj.args[0], "11")
     }
     
+    @Test
+    fun `Equal Str`() {
+        val where: WhereBuilder.()->Unit = { shName equal "russiaIsATerroristState" }
+        val whereObj = WhereBuilder().apply(where)
+        
+        assertEquals(whereObj.clauses[0], DbConstants.Name + "=?")
+        assertEquals(whereObj.args[0], "russiaIsATerroristState")
+    }
+    
+    
+    @Test
+    fun `Equal Bool`() {
+        val whereObj = WhereBuilder().apply { shActive equal true }
+        
+        assertEquals(whereObj.clauses[0], "active=?")
+        assertEquals(whereObj.args[0], "1")
+        
+        val whereObj2 = WhereBuilder().apply { shActive equal false }
+        
+        assertEquals(whereObj2.clauses[0], "active=?")
+        assertEquals(whereObj2.args[0], "0")
+    }
+    
+    
+    @Test
+    fun `Equal Long`() {
+        val whereObj = WhereBuilder().apply { lCol("shLong") equal 50L }
+        
+        assertEquals(whereObj.clauses[0], "shLong=?")
+        assertEquals(whereObj.args[0], "50")
+    }
+    
+    
+    @Test
+    fun `Equal Float`() {
+        val whereObj = WhereBuilder().apply { fCol("ruFloatSucks") equal 13.2F }
+        
+        assertEquals(whereObj.clauses[0], "ruFloatSucks=?")
+        assertEquals(whereObj.args[0], "13.2")
+    }
     
     
     
