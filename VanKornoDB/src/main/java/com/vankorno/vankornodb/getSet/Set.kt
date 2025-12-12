b2f61728a32941ab02d5dc1f594c56458c296b7b
VanKorno <kornovandrey@gmail.com>
2025-12-12 09:31:14 +0200

    Add type-safe functions

--- VanKornoDB/src/main/java/com/vankorno/vankornodb/getSet/Set.kt
+++ VanKornoDB/src/main/java/com/vankorno/vankornodb/getSet/Set.kt
@@ -6,9 +6,41 @@ import android.content.ContentValues
 import android.database.sqlite.SQLiteDatabase
 import com.vankorno.vankornodb.api.WhereBuilder
 import com.vankorno.vankornodb.core.data.DbConstants.ID
+import com.vankorno.vankornodb.dbManagement.data.BlobCol
+import com.vankorno.vankornodb.dbManagement.data.BoolCol
+import com.vankorno.vankornodb.dbManagement.data.FloatCol
+import com.vankorno.vankornodb.dbManagement.data.IntCol
+import com.vankorno.vankornodb.dbManagement.data.LongCol
+import com.vankorno.vankornodb.dbManagement.data.StrCol
+
+fun SQLiteDatabase.setInt(value: Int, table: String, column: IntCol, where: WhereBuilder.()->Unit) = setNoty(value, table, column.name, where)
+fun SQLiteDatabase.setStr(value: String, table: String, column: StrCol, where: WhereBuilder.()->Unit) = setNoty(value, table, column.name, where)
+fun SQLiteDatabase.setBool(value: Boolean, table: String, column: BoolCol, where: WhereBuilder.()->Unit) = setNoty(value, table, column.name, where)
+fun SQLiteDatabase.setLong(value: Long, table: String, column: LongCol, where: WhereBuilder.()->Unit) = setNoty(value, table, column.name, where)
+fun SQLiteDatabase.setFloat(value: Float, table: String, column: FloatCol, where: WhereBuilder.()->Unit) = setNoty(value, table, column.name, where)
+fun SQLiteDatabase.setBlob(value: ByteArray, table: String, column: BlobCol, where: WhereBuilder.()->Unit) = setNoty(value, table, column.name, where)
+
+fun SQLiteDatabase.setIntById(value: Int, id: Int, table: String, column: IntCol, andWhere: WhereBuilder.()->Unit) = setNoty(value, table, column.name, andWhere)
+fun SQLiteDatabase.setStrById(value: String, id: Int, table: String, column: StrCol, andWhere: WhereBuilder.()->Unit) = setNoty(value, table, column.name, andWhere)
+fun SQLiteDatabase.setBoolById(value: Boolean, id: Int, table: String, column: BoolCol, andWhere: WhereBuilder.()->Unit) = setNoty(value, table, column.name, andWhere)
+fun SQLiteDatabase.setLongById(value: Long, id: Int, table: String, column: LongCol, andWhere: WhereBuilder.()->Unit) = setNoty(value, table, column.name, andWhere)
+fun SQLiteDatabase.setFloatById(value: Float, id: Int, table: String, column: FloatCol, andWhere: WhereBuilder.()->Unit) = setNoty(value, table, column.name, andWhere)
+fun SQLiteDatabase.setBlobById(value: ByteArray, id: Int, table: String, column: BlobCol, andWhere: WhereBuilder.()->Unit) = setNoty(value, table, column.name, andWhere)
 
 
-fun SQLiteDatabase.setByIdNoty(value: Any, id: Int, table: String, column: String) = setNoty(value, table, column, ID, id)
+
+
+
+
+fun SQLiteDatabase.setByIdNoty(                                   value: Any,
+                                                                     id: Int,
+                                                                  table: String,
+                                                                 column: String,
+                                                               andWhere: WhereBuilder.()->Unit = {},
+) = setNoty(value, table, column) {
+    ID equal id
+    andGroup(andWhere)
+}
 
 
 fun <T> SQLiteDatabase.setNoty(                                                    value: Any,
