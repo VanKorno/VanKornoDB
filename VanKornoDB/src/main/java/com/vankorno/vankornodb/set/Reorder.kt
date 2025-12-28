fe02f31a08395c88f9548ae92e720c986be8d91a
VanKorno <kornovandrey@gmail.com>
2025-12-28 18:11:03 +0200

    Add reorder fun

---
+++ VanKornoDB/src/main/java/com/vankorno/vankornodb/set/Reorder.kt
@@ -0,0 +1,90 @@
+// region License
+/** This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ *  If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+**/
+// endregion
+package com.vankorno.vankornodb.set
+
+import android.database.sqlite.SQLiteDatabase
+import android.util.Log
+import com.vankorno.vankornodb.core.data.DbConstants.DbTAG
+import com.vankorno.vankornodb.get.getCursorPro
+import com.vankorno.vankornodb.get.getInt
+import com.vankorno.vankornodb.get.getLastPosition
+import com.vankorno.vankornodb.get.getTableIntsPro
+import com.vankorno.vankornodb.misc.columns
+import com.vankorno.vankornodb.misc.data.SharedCol.cID
+import com.vankorno.vankornodb.misc.data.SharedCol.cPosition
+
+fun SQLiteDatabase.reorder(                                                 table: String,
+                                                                               id: Int,
+                                                                     moveUpOrBack: Boolean,
+                                                                  makeFirstOrLast: Boolean = false,
+) {
+    // region LOG
+    Log.d(DbTAG, "reorder(table = $table, id = $id, moveUpOrBack = $moveUpOrBack, makeFirstOrLast = $makeFirstOrLast")
+    // endregion
+    if (makeFirstOrLast) {
+        reorderToStartEnd(table, id, moveUpOrBack)
+        return //\/\/\/\/\/\
+    }
+    val position = getInt(table, cPosition) { ID = id }
+    
+    getCursorPro(table, columns(cID, cPosition)) {
+        where {
+            if (moveUpOrBack)
+                cPosition less position
+            else
+                cPosition greater position
+        }
+        orderByPosition()
+        limit = 1
+    }.use { cursor ->
+        if (cursor.count < 1)  return  //\/\/\/\/\/\
+        
+        if (moveUpOrBack)
+            cursor.moveToLast()
+        else
+            cursor.moveToFirst()
+        
+        val neighbourID = cursor.getInt(0)
+        val neighbourPosition = cursor.getInt(1)
+        
+        setInt(neighbourPosition, table, cPosition) { ID = id }
+        setInt(position, table, cPosition) { ID = neighbourID }
+    }
+}
+
+
+private fun SQLiteDatabase.reorderToStartEnd(                                      table: String,
+                                                                                      id: Int,
+                                                                            moveUpOrBack: Boolean,
+) {
+    if (!moveUpOrBack) {
+        val newPosition = getLastPosition(table) + 1
+        setInt(newPosition, table, cPosition) { ID = id }
+    }
+    else {
+        val position = getInt(table, cPosition) { ID = id }
+        
+        val precedingTaskIDsPositions = getTableIntsPro(table, arrayOf(cID, cPosition)) {
+            where { cPosition less position }
+            orderByPosition()
+        }
+        
+        setInt(1, table, cPosition) { ID = id }
+        
+        for (idPosition in precedingTaskIDsPositions) {
+            val taskID = idPosition[0]
+            val newPosition = idPosition[1] + 1
+            setInt(newPosition, table, cPosition) { ID = taskID }
+        }
+    }
+}
+
+
+
+
+
+
+
