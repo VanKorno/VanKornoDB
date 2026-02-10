7fc70e8eb347f7eefae50bf09ab6faea93e4d38b
VanKorno <71512024+VanKorno@users.noreply.github.com>
2026-02-10 11:39:33 +0200

    Remove redundant Coroutine stuff and some fun

--- VanKornoDB/src/main/java/com/vankorno/vankornodb/dbManagement/DbReaderWriter.kt
+++ VanKornoDB/src/main/java/com/vankorno/vankornodb/dbManagement/DbReaderWriter.kt
@@ -15,7 +15,6 @@ import com.vankorno.vankornodb.dbManagement.data.BaseEntityMeta
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 
 abstract class DbReaderWriter(
@@ -37,130 +36,26 @@ abstract class DbReaderWriter(
     runOnCreate = onCreate,
     runOnUpgrade = onUpgrade,
 ) {
+    /** A shared fun */
+    private inline fun writeBase(                                           funName: String = "write",
+                                                            crossinline run: (SQLiteDatabase)->Unit,
+    ) {
+        try {
+            lock.withLock {
+                currDb.transaction { run(this) }
+            }
+        } catch (e: Exception) {
+            // region LOG
+                Log.e(DbTAG, "$funName() failed. Details: ${e.message}", e)
+            // endregion
+        }
+    }
+    
+    
     /**
      * Better reading performance, optimal for reading, but writing can also be done in a less safe way.
      */
-    @JvmOverloads
-    fun <T> read(                                                 defaultValue: T,
-                                                                       funName: String = "read",
-                                                                           run: (SQLiteDatabase)->T,
-    ): T = runBlocking {
-        withContext(Dispatchers.IO) {
-            readBase(defaultValue, funName, run)
-        }
-    }
-    
-    /**
-     * Same as read(), but does not return anything. Useful for reading db and setting some values from the inside.
-     * Can launch its own fire-and-forget coroutine if [async] is true.
-     */
-    @JvmOverloads
-    fun voidRead(                                                   funName: String = "voidRead",
-                                                                      async: Boolean = false,
-                                                                        run: (SQLiteDatabase)->Unit,
-    ) {
-        if (async) {
-            CoroutineScope(Dispatchers.IO).launch {
-                readBase(Unit, funName) { run(it) }
-            }
-        } else {
-            runBlocking {
-                withContext(Dispatchers.IO) {
-                    readBase(Unit, funName) { run(it) }
-                }
-            }
-        }
-    }
-    
-    /**
-     * Void (can read or write db, but doesn't return anything), with writing overhead.
-     * Can launch its own fire-and-forget coroutine if [async] is true.
-     */
-    @JvmOverloads
-    fun write(                                                      funName: String = "write",
-                                                                      async: Boolean = false,
-                                                                        run: (SQLiteDatabase)->Unit,
-    ) {
-        if (async) {
-            CoroutineScope(Dispatchers.IO).launch {
-                writeBase(funName, run)
-            }
-        } else {
-            runBlocking {
-                withContext(Dispatchers.IO) {
-                    writeBase(funName, run)
-                }
-            }
-        }
-    }
-    
-    /**
-     * All-mighty, but with writing overhead.
-     */
-    @JvmOverloads
-    fun <T> readWrite(                                         defaultValue: T,
-                                                                    funName: String = "readWrite",
-                                                                        run: (SQLiteDatabase)->T,
-    ): T = runBlocking {
-        withContext(Dispatchers.IO) {
-            readWriteBase(defaultValue, funName, run)
-        }
-    }
-    
-    
-    
-    
-    
-    
-    // =================================  S U S P E N D E D  ================================= \\
-    
-    /**
-     * Better reading performance, optimal for reading, but writing can also be done in a less safe way.
-     * Suspending non-blocking version (to be used inside coroutines)
-     */
-    suspend fun <T> readSusp(                                   defaultValue: T,
-                                                                     funName: String = "readSusp",
-                                                                         run: (SQLiteDatabase)->T,
-    ): T = withContext(Dispatchers.IO) {
-        readBase(defaultValue, funName, run)
-    }
-    
-    /**
-     * Same as readDB, but does not return anything (for reading db and setting some values from the inside).
-     * Suspending non-blocking version (to be used inside coroutines)
-     */
-    suspend fun voidReadSusp(                                     funName: String = "voidReadSusp",
-                                                                      run: (SQLiteDatabase)->Unit,
-    ) = withContext(Dispatchers.IO) {
-        readBase(Unit, funName){ run(it) }
-    }
-    
-    /**
-     * Void (can read or write db, but doesn't return anything), with writing overhead.
-     * Suspending non-blocking version (to be used inside coroutines)
-     */
-    suspend fun writeSusp(                                          funName: String = "writeSusp",
-                                                                        run: (SQLiteDatabase)->Unit,
-    ) = withContext(Dispatchers.IO) {
-        writeBase(funName) { run(it) }
-    }
-    
-    /**
-     * All-mighty, but with writing overhead.
-     * Suspending non-blocking version (to be used inside coroutines)
-     */
-    suspend fun <T> readWriteSusp(                          defaultValue: T,
-                                                                 funName: String = "readWriteSusp",
-                                                                     run: (SQLiteDatabase)->T,
-    ): T = withContext(Dispatchers.IO) {
-        readWriteBase(defaultValue, funName, run)
-    }
-    
-    
-    
-    // ====================================  B A S E D  ==================================== \\
-    
-    inline fun <T> readBase(                                      defaultValue: T,
+    inline fun <T> read(                                          defaultValue: T,
                                                                        funName: String = "read",
                                                                crossinline run: (SQLiteDatabase)->T,
     ): T {
@@ -176,26 +71,49 @@ abstract class DbReaderWriter(
         }
     }
     
-    inline fun voidReadBase(                                        funName: String = "voidRead",
-                                                            crossinline run: (SQLiteDatabase)->Unit,
-    ) = readBase(Unit, funName) { run(it) }
     
-    
-    inline fun writeBase(                                           funName: String = "write",
-                                                            crossinline run: (SQLiteDatabase)->Unit,
+    /**
+     * Same as read(), but does not return anything. Useful for reading db and setting some values from the inside.
+     * Can launch its own fire-and-forget coroutine if [async] is true.
+     */
+    @JvmOverloads
+    fun voidRead(                                                   funName: String = "voidRead",
+                                                                      async: Boolean = false,
+                                                                        run: (SQLiteDatabase)->Unit,
     ) {
-        try {
-            lock.withLock {
-                currDb.transaction { run(this) }
+        if (async) {
+            CoroutineScope(Dispatchers.IO).launch {
+                read(Unit, funName) { run(it) }
             }
-        } catch (e: Exception) {
-            // region LOG
-                Log.e(DbTAG, "$funName() failed. Details: ${e.message}", e)
-            // endregion
+        } else {
+            read(Unit, funName) { run(it) }
         }
     }
     
-    inline fun <T> readWriteBase(                                defaultValue: T,
+    
+    /**
+     * Void (can read or write db, but doesn't return anything), with writing overhead.
+     * Can launch its own fire-and-forget coroutine if [async] is true.
+     */
+    @JvmOverloads
+    fun write(                                                      funName: String = "write",
+                                                                      async: Boolean = false,
+                                                                        run: (SQLiteDatabase)->Unit,
+    ) {
+        if (async) {
+            CoroutineScope(Dispatchers.IO).launch {
+                writeBase(funName, run)
+            }
+        } else {
+            writeBase(funName, run)
+        }
+    }
+    
+    
+    /**
+     * All-mighty, but with writing overhead.
+     */
+    inline fun <T> readWrite(                                    defaultValue: T,
                                                                       funName: String = "readWrite",
                                                               crossinline run: (SQLiteDatabase)->T,
     ): T {
@@ -210,6 +128,62 @@ abstract class DbReaderWriter(
             defaultValue
         }
     }
+    
+    
+    
+    
+    
+    // =================================  S U S P E N D E D  ================================= \\
+    
+    /**
+     * Better reading performance, optimal for reading, but writing can also be done in a less safe way.
+     * Suspending non-blocking version (to be used inside coroutines)
+     */
+    suspend fun <T> readSusp(                                   defaultValue: T,
+                                                                     funName: String = "readSusp",
+                                                                         run: (SQLiteDatabase)->T,
+    ): T = withContext(Dispatchers.IO) {
+        read(defaultValue, funName, run)
+    }
+    
+    
+    /**
+     * Same as readDB, but does not return anything (for reading db and setting some values from the inside).
+     * Suspending non-blocking version (to be used inside coroutines)
+     */
+    suspend fun voidReadSusp(                                     funName: String = "voidReadSusp",
+                                                                      run: (SQLiteDatabase)->Unit,
+    ) = withContext(Dispatchers.IO) {
+        read(Unit, funName){ run(it) }
+    }
+    
+    
+    /**
+     * Void (can read or write db, but doesn't return anything), with writing overhead.
+     * Suspending non-blocking version (to be used inside coroutines)
+     */
+    suspend fun writeSusp(                                          funName: String = "writeSusp",
+                                                                        run: (SQLiteDatabase)->Unit,
+    ) = withContext(Dispatchers.IO) {
+        writeBase(funName) { run(it) }
+    }
+    
+    
+    /**
+     * All-mighty, but with writing overhead.
+     * Suspending non-blocking version (to be used inside coroutines)
+     */
+    suspend fun <T> readWriteSusp(                          defaultValue: T,
+                                                                 funName: String = "readWriteSusp",
+                                                                     run: (SQLiteDatabase)->T,
+    ): T = withContext(Dispatchers.IO) {
+        readWrite(defaultValue, funName, run)
+    }
+    
+    
+    
+    
+    
     
     
     
