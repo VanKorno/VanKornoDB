c1834f693bf6551c74fb68fd97739b0a92b77203
VanKorno <71512024+VanKorno@users.noreply.github.com>
2026-02-16 23:00:54 +0200

    Better test detection for the logger

--- VanKornoDB/src/main/java/com/vankorno/vankornodb/misc/Logger.kt
+++ VanKornoDB/src/main/java/com/vankorno/vankornodb/misc/Logger.kt
@@ -4,15 +4,13 @@ import android.util.Log
 import com.vankorno.vankornodb.core.data.DbConstants.DbTAG
 
 private val unitTestCase: Boolean by lazy {
-    val testsRun = Thread.currentThread().stackTrace.any { it.className.startsWith("org.junit.") }
-    
     val androidTestsRun = try {
         Class.forName("androidx.test.platform.app.InstrumentationRegistry")
         true
     } catch (_: ClassNotFoundException) {
         false
     }
-    testsRun && !androidTestsRun
+    !androidTestsRun  &&  Thread.currentThread().stackTrace.any { it.className.startsWith("org.junit.") }
 }
 
 
